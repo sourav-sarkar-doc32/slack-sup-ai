df1f899af1982805febf8c7f665f8f6b6c519689
sourav-sarkar-doc32 <sourav@doc32.in>
2024-03-15 18:43:03 +0400

    basic login flow working

---
+++ app/dashboard/page.js
@@ -0,0 +1,27 @@
+// pages/dashboard.js
+"use client";
+import { useSession, signOut } from 'next-auth/react';
+
+export default function Dashboard() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-700 dark:text-gray-200">
+        <div className="max-w-4xl mx-auto py-12">
+          <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+          <p className="mb-2">You're logged in as <strong>{session.user.email}</strong></p>
+          <button
+            onClick={() => signOut()}
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Sign out
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  // Redirect or show a message if not authenticated
+  return <p>Not Logged in...<a href="/">Goto Home Page</a></p>;
+}
